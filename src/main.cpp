e76e1cd5aa25c061077d19bee9c755022743acb8
Oliver Braun <oliverbraun89@gmail.com>
2018-09-16 15:46:50 +0200

    feat: Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
 #include <SFML/Graphics.hpp>
 
+unsigned int windowWidth = 1200;
+unsigned int windowHeight = 800;
+
 int main() {
+  sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Snake");
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while(window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+    window.display();
+  }
+
   return 0;
 }
