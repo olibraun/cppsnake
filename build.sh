b4b1c5b30ba52981b4b5d87f77b12936b7660252
Oliver Braun <oliverbraun89@gmail.com>
2018-09-23 20:07:33 +0200

    feat: -O2 compiler optimization
    According to internet research:
    * -O2 is the default recommended optimization level
    * the optimization option should be given to all compile steps and the linker

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ -c ./src/*.cpp
-g++ -c ./src/*/*.cpp
-g++ ./*.o -o ./bin/snake -lsfml-graphics -lsfml-window -lsfml-system
+g++ -O2 -c ./src/*.cpp
+g++ -O2 -c ./src/*/*.cpp
+g++ -O2 ./*.o -o ./bin/snake -lsfml-graphics -lsfml-window -lsfml-system
 rm *.o
