760a2ea321478909456283562f779b730710b7b2
Oliver Braun <oliverbraun89@gmail.com>
2018-09-27 17:41:41 +0200

    feat: Timer in build script

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+SECONDS=0
+
+echo "Starting to build Snake."
+
 g++ -O2 -c ./src/*.cpp
 g++ -O2 -c ./src/*/*.cpp
 g++ -O2 ./*.o -o ./bin/snake -lsfml-graphics -lsfml-window -lsfml-system
 rm *.o
+
+echo "Done, time elapsed: $SECONDS seconds."
